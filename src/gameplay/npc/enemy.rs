80aae60566a156b9507623becbc024541c161705
Jan Hohenheim <jan@hohenheim.ch>
2026-02-11 19:07:53 +0100

    WIP

--- src/gameplay/npc/enemy.rs
+++ src/gameplay/npc/enemy.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 use bevy_bae::prelude::*;
+use bevy_landmass::Archipelago3d;
+use bevy_trenchbroom::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
 	app.add_systems(FixedUpdate, update_sensors.before(BaeSystems::ExecutePlan));
@@ -25,6 +27,23 @@ fn enemy_htn(
 fn walk_randomly(
 	In(input): In<OperatorInput>,
 	mut transforms: Query<&mut Transform>,
+	archipelago: Single<&Archipelago3d>,
 ) -> OperatorStatus {
+	let offset = Circle::new(3.0);
+	archipelago.sample_point(point, point_sample_distance);
 	OperatorStatus::Ongoing
+}
+
+#[point_class(base(Transform, Visibility))]
+struct MyCoolEmitter {
+	some_property: String,
+	some_other_property: i32,
+}
+impl Default for MyCoolEmitter {
+	fn default() -> Self {
+		Self {
+			some_property: "default".to_string(),
+			some_other_property: 0,
+		}
+	}
 }
