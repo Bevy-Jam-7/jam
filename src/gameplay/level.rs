92485c7e940c6a58fff90fe7db7519f09c89483c
Jan Hohenheim <jan@hohenheim.ch>
2026-02-14 18:03:58 +0100

    Fix player stuck

--- src/gameplay/level.rs
+++ src/gameplay/level.rs
@@ -199,11 +199,25 @@ pub(crate) fn spawn_level(
 		}
 		CurrentLevel::Commune => {
 			let level_three_assets = level_three_assets.expect("If we don't have level three assets when spawning level three, we're in deep shit. Sorry player, we bail here.");
+
+			let archipelago = commands
+				.spawn((
+					Name::new("Main Level Archipelago"),
+					DespawnOnExit(Screen::Gameplay),
+					Archipelago3d::new(ArchipelagoOptions::from_agent_radius(NPC_RADIUS)),
+				))
+				.id();
+
 			commands.spawn((
 				Name::new("Level"),
 				SceneRoot(level_three_assets.level.clone()),
 				DespawnOnExit(Screen::Gameplay),
 				Level,
+				Island3dBundle {
+					island: Island,
+					archipelago_ref: ArchipelagoRef3d::new(archipelago),
+					nav_mesh: NavMeshHandle3d(level_three_assets.navmesh.clone()),
+				},
 			));
 		}
 		CurrentLevel::Train => {
@@ -499,7 +513,7 @@ pub(crate) struct LevelThreeAssets {
 	#[dependency]
 	pub(crate) level: Handle<Scene>,
 	// #[dependency]
-	// pub(crate) navmesh: Handle<Navmesh>,
+	pub(crate) navmesh: Handle<Navmesh>,
 }
 impl FromWorld for LevelThreeAssets {
 	fn from_world(world: &mut World) -> Self {
@@ -509,7 +523,7 @@ impl FromWorld for LevelThreeAssets {
 			// Our main level is inspired by the TheDarkMod fan mission [Volta I: The Stone](https://www.thedarkmod.com/missiondetails/?internalName=volta1_3)
 			level: assets.load("maps/main/three/three.map#Scene"),
 			// You can regenerate the navmesh by using `bevy_rerecast_editor`
-			// navmesh: assets.load("maps/main/three/three.nav"),
+			navmesh: assets.load("maps/main/three/three.nav"),
 		}
 	}
 }
