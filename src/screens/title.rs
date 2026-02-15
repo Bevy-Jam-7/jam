f375460d42386992e536146e9c2344b6e9126d19
Joona Aalto <jondolf.dev@gmail.com>
2026-02-15 20:36:23 +0200

    Reduce gloop
    I need your help! Can you come here?
    
    Well, I can't. I'm buying clothes.
    
    Alright, hurry up then come over here.
    
    I can't find them.
    
    What do you mean you can't find them?
    
    I can't find them. There's only gloop.
    
    Whaddya mean "there's only gloop"?
    
    It mean there's only gloop.
    
    Well get out of the gloop aisle!
    
    Alright, you don't have to shout at me (moves to the next aisle) There's more gloop!
    
    Whaddya mean "there's more gloop"?!
    
    There's just more gloop!
    
    Go into the next aisle!
    
    (moves to the next aisle) There's still gloop!
    
    Where are you right now?!
    
    I'm at gloop!
    
    WHADDYA MEAN YOU'RE "AT GLOOP"!?
    
    I MEAN I'M AT GLOOP!
    
    WHAT STORE ARE YOU IN?!
    
    I'M AT THE GLOOP STORE!!
    
    WHY ARE YOU BUYING CLOTHES AT THE GLOOP STORE?!
    
    FUCK YOU!!

--- src/screens/title.rs
+++ src/screens/title.rs
@@ -1,11 +1,12 @@
 //! The title screen that appears after the splash screen.
 
 use bevy::prelude::*;
+use bevy_seedling::sample::SamplePlayer;
 
-use crate::{menus::Menu, screens::Screen};
+use crate::{audio::MusicPool, menus::Menu, screens::Screen};
 
 pub(super) fn plugin(app: &mut App) {
-	app.add_systems(OnEnter(Screen::Title), open_main_menu);
+	app.add_systems(OnEnter(Screen::Title), (open_main_menu, spawn_gloop));
 	app.add_systems(OnExit(Screen::Title), close_menu);
 }
 
@@ -15,4 +16,12 @@ fn open_main_menu(mut next_menu: ResMut<NextState<Menu>>) {
 
 fn close_menu(mut next_menu: ResMut<NextState<Menu>>) {
 	next_menu.set(Menu::None);
+}
+
+fn spawn_gloop(mut commands: Commands, assets: Res<AssetServer>) {
+	commands.spawn((
+		DespawnOnEnter(Screen::Gameplay),
+		SamplePlayer::new(assets.load("audio/music/gloopy.ogg")).looping(),
+		MusicPool,
+	));
 }
