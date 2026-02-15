34f570d55da306a3d252173ec0257ab2d2902e12
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2026-02-15 19:18:18 +0100

    use standard label

--- src/menus/settings.rs
+++ src/menus/settings.rs
@@ -191,7 +191,7 @@ impl QualitySettingsButton {
 			),));
 		} else {
 			cmd.entity(ctx.entity)
-				.insert(widget::label_small("Change in main menu"));
+				.insert(widget::label("Change in Main menu"));
 		}
 	}
 }
